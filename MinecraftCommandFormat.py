cd525550f5690685b3d898253e728d26bc8681b5
destruc7i0n <dscdsouza@outlook.com>
2015-03-19 15:37:05 -0400

    Finished

--- MinecraftCommandFormat.py
+++ MinecraftCommandFormat.py
@@ -3,12 +3,12 @@ import sublime_plugin
 
 class MinecraftFormatBaseCommand(sublime_plugin.TextCommand):
 
-	"""docstring for MinecraftFormatBaseCommand"""
+	""" Base code | Majority is from @TexelElf """
 
-	def indent(ct):
+	def indent(self, ct):
 		return "".join(["\t" for i in xrange(ct)])
 
-	def strexplode(command):
+	def strexplode(self, command):
 		coms = []
 		if not command:
 			return coms
@@ -68,8 +68,29 @@ class MinecraftFormatBaseCommand(sublime_plugin.TextCommand):
 			if line:
 				coms.append(self.indent(i)+line+"\n")
 		return coms
-		
 
+
+	def strcollapse(self, lines):
+		commands = []
+		command = ""
+		id = ""
+		comfound = True
+		for l in lines:
+			if not l:
+				continue
+			if l[0] == ";":
+				continue
+			if comfound:
+				comfound = False
+				command += l.lstrip().rstrip() + " "
+			else:
+				command += l.lstrip().rstrip()
+		else:
+			commands.append((id,command.decode("unicode-escape")))
+		
+			
+		return commands
+				
 class MinecraftFormatCommand(MinecraftFormatBaseCommand):
 	
 	""" Pretty Print a Minecraft Command """
@@ -85,15 +106,33 @@ class MinecraftFormatCommand(MinecraftFormatBaseCommand):
 				selection = region
 
 			fs = self.view.substr(selection)
-			print fs
 
 			if "{" in fs:
 				mdatapos = fs.find("{")
 				outputlines.append(fs[:mdatapos]+"\n")
 				outputlines+=self.strexplode(fs[mdatapos:])
 			else:
-				outputlines.append(str(fs)+"\n")			
+				outputlines.append(str(fs)+"\n")
+			outputlines.append("\n")		
 #			obj = self.strexplode(self.view.substr(selection))
 			self.view.replace(edit, selection, ("".join(outputlines)))
 
+class MinecraftUnFormatCommand(MinecraftFormatBaseCommand):
 
+	""" Bring Pretty Printed Command Back To String """
+
+	def run(self, edit):
+		for region in self.view.sel():
+
+			# If no selection, use the entire file as the selection
+			if region.empty():
+				selection = sublime.Region(0, self.view.size())
+			else:
+				selection = region
+
+			fs = self.view.substr(selection).splitlines(True)
+			print fs
+
+			output = self.strcollapse(fs)[0][1]
+			print output
+			self.view.replace(edit, selection, output)
